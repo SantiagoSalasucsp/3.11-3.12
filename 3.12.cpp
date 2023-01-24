bdf16b29d83d975a29b51a856304e88155211069
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2023-01-24 00:28:31 -0500

    Update 3.12.cpp

--- 3.12.cpp
+++ 3.12.cpp
@@ -1,53 +1,7 @@
 #include <iostream>
 #include <string>
+#include "Date.h"
 using namespace std;
-
-class Date {
-public:
-  Date(int day_, int month_, int year_) {
-    if (day_>0&&day_<=30)
-      day = day_;
-    year = year_;
-    if (month >= 0 && month <= 13) {
-      month = month_;
-    }
-  }
-
-
-  void setday(int day_) {
-    day = day_; 
-    }
-
-  int getday() const{
-    return day;
-      }
-    
-
-  void setmonth(int month_) {
-    if (month_ <= 12 && 1 <= month_) {
-      month = month_;
-    }
-  }
-
-  int getmonth() const {
-    return month; 
-    }
-
-  void setyear(int year_) {
-    year = year_; 
-    }
-
-  int getyear() const {
-    return year; 
-    };
-
-  
-
-private:
-  int month;
-  int day;
-  int year;
-};
 
 int main(){
   Date dia{18,2,2023};
